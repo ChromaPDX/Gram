def6a01dee6afd18e6326429b5a6f910327ed528
robbykraft <robbykraft@gmail.com>
2013-12-20 12:18:27 -0800

    Initial Commit

---
+++ Gram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gram
+//
+//  Created by Robby Kraft on 12/20/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gram
+//
+//  Created by Robby Kraft on 12/20/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
